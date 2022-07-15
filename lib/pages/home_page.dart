fcc6a5a71a30f1efd6a41f92ceff67279c84671c
mohammadaboelnasr <m.aboelnasr@live.com>
2022-07-15 18:11:24 +0200

    base

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WeatherApp'),
+        centerTitle: false,
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(30.0),
+        child: Center(
+          child: Text(
+            'There is no weather 😔 , start searching now 🔍',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
